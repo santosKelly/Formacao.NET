02ec817cc33b720aad52f8d8d382aa58af6605b9
Kelly <73318640+santosKelly@users.noreply.github.com>
2023-08-09 11:07:49 -0300

    Update Program.cs

--- excecoesEcolecoes/Program.cs
+++ excecoesEcolecoes/Program.cs
@@ -1,31 +1,5 @@
 ﻿using excecoesEcolecoes.Models;
 
-
-//string[] linhas = File.ReadAllLines("C:\\Users\\kelly\\source\\repos\\excecoesEcolecoes\\excecoesEcolecoes\\Arquivos\\arquivoLeitura.txt");
-
-//foreach (string linha in linhas)
-//{
-//    Console.WriteLine(linha);
-//}
-
-
-//LeituraArquivo arquivo = new LeituraArquivo();
-
-//var (sucesso, linhasArquivo, _) = arquivo.LerArquivo("C:\\Users\\kelly\\source\\repos\\excecoesEcolecoes\\excecoesEcolecoes\\Arquivos\\arquivoLeitura.txt");
-
-//if (sucesso)
-//{
-
-//    foreach (string linha in linhasArquivo)
-//    {
-//        Console.WriteLine(linha);
-//    }
-//}
-//else
-//{
-//    Console.WriteLine("Não foi possível ler o arquivo");
-//}
-
 Queue<int> fila = new Queue<int>();
 
 fila.Enqueue(2);
